f59198898b97c932dd1a1e2e0bffe7cf273a94a4
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-10-01 00:46:20 +0300

    Print command line

--- src/trac.asm
+++ src/trac.asm
@@ -1,46 +1,94 @@
+; Windows-specific stuff, entry point
+;
+; This file is part of Triton-A project.
+; Copyright 2019, Dmitry Grigoryev
 
 format PE console
 entry start
 
-section '.data' data readable writable
-        hello_str db 'Hello, world!', 0ah
-        hello_len = $-hello_str
+section '.rdata' data readable
+include 'messages.inc'
 
-        align 4
+section '.data' data readable writable
         stdin dd ?
         bytes_written dd ?
+        exit_code dd ?
 
 section '.text' code executable readable
 
+; Entry point
 start:
+        nop
+        cld ; Clear direction flag according to calling conventions
+
         push -11
         call [GetStdHandle]
         mov [stdin], eax
 
+        call [GetCommandLineA]
+
+        ; Calculate length of command line
+        push eax
+        push eax
+        call length
+        add esp, 4
+
+        ; Call driver
+        pop ecx
+        push eax
+        push ecx
+        call drive
+        add esp, 8
+        mov [exit_code], eax
+
+        push [exit_code]
+        call [ExitProcess]
+
+; Prints string to stdandard output
+; In: 1) Address of string
+;     2) Length of string
+print:
+        push ebp
+        mov ebp, esp
         push 0
         push bytes_written
-        push hello_len
-        push hello_str
+        push dword [ebp+12]
+        push dword [ebp+8]
         push [stdin]
         call [WriteFile]
+        mov esp, ebp
+        pop ebp
+        ret
 
+; Prints newline to stdandard output
+newline:
         push 0
-        call [ExitProcess]
+        push bytes_written
+        push dword newline_len
+        push dword newline_str
+        push [stdin]
+        call [WriteFile]
+        ret
+
+include 'driver.inc'
+include 'string.inc'
 
 section '.idata' import data readable writable
 
-        dd 0, 0, -1, rva kernel32.name, rva kernel32.iat
+        dd 0, 0, 0, rva kernel32.name, rva kernel32.iat
         dd 0, 0, 0, 0, 0
 
         kernel32.name db 'kernel32.dll', 0
 
         align 4
-        label kernel32.iat dword
+    kernel32.iat:
         ExitProcess     dd rva ExitProcess.hint
+        GetCommandLineA dd rva GetCommandLineA.hint
         GetStdHandle    dd rva GetStdHandle.hint
         WriteFile       dd rva WriteFile.hint
         dd 0
 
         ExitProcess.hint        db 0, 0, 'ExitProcess', 0
+        GetCommandLineA.hint    db 0, 0, 'GetCommandLineA', 0
         GetStdHandle.hint       db 0, 0, 'GetStdHandle', 0
         WriteFile.hint          db 0, 0, 'WriteFile', 0
