0a045118a26f9b14a542cc67f41187543299d1f2
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-10-07 20:26:04 +0300

    Read input files

---
+++ test-cmd-args.bat
@@ -0,0 +1,7 @@
+call build.bat
+build\trac.exe -o build\a.exe test\x.tra test\y.tra
+build\trac.exe test\x.tra -o build\a.exe test\y.tra
+build\trac.exe test\x.tra test\y.tra
+build\trac.exe -o build\a.exe
+build\trac.exe -o
+build\trac.exe -i xyz
--- test.bat
+++ test.bat
@@ -1,6 +1,3 @@
 call build.bat
-build\trac.exe -o build\a.exe test\x.tra test\y.tra
-build\trac.exe test\x.tra test\y.tra
-build\trac.exe -o build\a.exe
-build\trac.exe -o
-build\trac.exe -i xyz
+build\trac.exe -o build\a.exe test\x.tra
+build\trac.exe -o build\b.exe test\x.tra test\y.tra
