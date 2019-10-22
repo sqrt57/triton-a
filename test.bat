87d83616961a508e4d086b0ac3c0821aeac0b2e1
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-10-22 07:25:37 +0300

    Renname lexemes to tokens

--- test.bat
+++ test.bat
@@ -6,3 +6,4 @@ build\trac.exe -o build\a.exe test\bad3.tra
 build\trac.exe -o build\a.exe test\bad4.tra
 build\trac.exe -o build\a.exe test\bad5.tra
 build\trac.exe -o build\a.exe test\bad6.tra
+build\trac.exe -o build\a.exe test\bad7.tra
