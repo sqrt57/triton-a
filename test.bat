60b5567a4f66ccac838a354e6da929aad0bf5195
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-10-22 23:37:58 +0300

    Lex string literal

--- test.bat
+++ test.bat
@@ -5,5 +5,3 @@ build\trac.exe -o build\a.exe test\bad2.tra
 build\trac.exe -o build\a.exe test\bad3.tra
 build\trac.exe -o build\a.exe test\bad4.tra
 build\trac.exe -o build\a.exe test\bad5.tra
-build\trac.exe -o build\a.exe test\bad6.tra
-build\trac.exe -o build\a.exe test\bad7.tra
