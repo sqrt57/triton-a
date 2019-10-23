7af06a62481e058934ce09016cc72c7cf069b4e6
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-10-23 21:10:05 +0300

    Process comments

--- test.bat
+++ test.bat
@@ -1,7 +1,3 @@
 call build.bat
 build\trac.exe -o build\a.exe test\x.tra
-build\trac.exe -o build\a.exe test\bad1.tra
-build\trac.exe -o build\a.exe test\bad2.tra
-build\trac.exe -o build\a.exe test\bad3.tra
-build\trac.exe -o build\a.exe test\bad4.tra
-build\trac.exe -o build\a.exe test\bad5.tra
+
