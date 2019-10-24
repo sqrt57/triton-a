2dd8efe468425d30d0c8f1cc2b7a57c5a38a87b5
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-10-24 22:30:24 +0300

    Cleanup

--- test-cmd-args.bat
+++
@@ -1,7 +0,0 @@
-call build.bat
-build\trac.exe -o build\a.exe test\x.tra test\y.tra
-build\trac.exe test\x.tra -o build\a.exe test\y.tra
-build\trac.exe test\x.tra test\y.tra
-build\trac.exe -o build\a.exe
-build\trac.exe -o
-build\trac.exe -i xyz
