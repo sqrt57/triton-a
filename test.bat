176c03c288d31ba8c323ae8be6fdccfd225e60ae
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-10-28 21:32:20 +0300

    Check for correct number literal end

--- test.bat
+++ test.bat
@@ -3,3 +3,4 @@ build\trac.exe -o build\a.exe test\x.tra
 build\trac.exe -o build\a.exe test\bad1.tra
 build\trac.exe -o build\a.exe test\bad2.tra
 build\trac.exe -o build\a.exe test\bad3.tra
+build\trac.exe -o build\a.exe test\bad4.tra
