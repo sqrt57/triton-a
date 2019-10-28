566cdcb0349b3f16de1476162b86358c9878afd1
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-10-28 21:33:40 +0300

    Hexadecimal number literals

--- test.bat
+++ test.bat
@@ -4,3 +4,6 @@ build\trac.exe -o build\a.exe test\bad1.tra
 build\trac.exe -o build\a.exe test\bad2.tra
 build\trac.exe -o build\a.exe test\bad3.tra
 build\trac.exe -o build\a.exe test\bad4.tra
+build\trac.exe -o build\a.exe test\bad5.tra
+build\trac.exe -o build\a.exe test\bad6.tra
+build\trac.exe -o build\a.exe test\bad7.tra
